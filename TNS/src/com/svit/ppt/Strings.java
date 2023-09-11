7bebbbec2c7fc719fe6f4a3021156fa22638a535
riya_52 <riya.p18702@gmail.com>
2023-09-11 08:09:55 +0530

    first commit uploading all files

---
+++ TNS/src/com/svit/ppt/Strings.java
@@ -0,0 +1,20 @@
+package com.svit.ppt;
+
+public class Strings {
+	public static void main(String[] args) {
+		String x = "Study";
+		String y = "easy";
+		String z = x.concat(y);
+		
+		z = z.replace("easy", "hard");
+		
+		System.out.println(z);
+		
+		if(z.equals("Studyeasy")){
+			System.out.println("Text is Studyeasy");
+		}else{
+			System.out.println("Text is not Studyeasy");
+		}
+	}
+
+}
---
+++ TNS/src/com/svit/ppt/variables.java
@@ -0,0 +1,10 @@
+package com.svit.ppt;
+
+public class variables {
+	public static void main(String[] args) {
+		   int value1;
+		   value1=1;
+		   System.out.println(value1);
+	}
+
+}
