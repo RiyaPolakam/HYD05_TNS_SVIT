875c46b29963316c0b902cbae4cd639508e62c04
riya_52 <riya.p18702@gmail.com>
2023-09-12 08:18:51 +0530

    daythree

---
+++ TNS/src/com/svit/staticinstance/daythree/Test.java
@@ -0,0 +1,16 @@
+package com.svit.staticinstance.daythree;
+
+public class Test {
+	public static void main(String args[ ])  {
+	       Base obj1 = new Derived();
+	        
+	       // As per overriding rules this should call to class Derive's static
+	       // overridden method. Since static method can not be overridden, it
+	       // calls Base's display()
+	       Base.display(); 
+	        
+	       // Here overriding works and Derive's print() is called
+	       obj1.print();
+	       }
+
+}
